de6f42e4b11276518238461b37bdbb666c4f580f
Andreas <anfauske@gmail.com>
2019-06-01 14:46:34 +0200

    Added card screen.
    Co-authored-by: Daniel Bjørnbakk <daniel.bjornbakk@gmail.com>

--- lib/components/project_card.dart
+++ lib/components/project_card.dart
@@ -6,15 +6,74 @@ class ProjectCard extends StatelessWidget {
 
   ProjectCard(this.project);
 
+  Widget _buildHeader(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 17, left: 23),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            project.name,
+            style: Theme.of(context).textTheme.headline,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 4.0, bottom: 16.0),
+            child: Text(project.developer.name,
+                style: Theme.of(context).textTheme.subhead),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Container(child: Text(project.name));
     return InkWell(
       onTap: () {
-        Navigator.of(context).pushNamed('/details', arguments: project);
+        Navigator.of(context).pushNamed(
+          '/details',
+          arguments: Project(name: "Project Name"),
+        );
       },
       child: Card(
-        child: Text(project.name),
+        elevation: 2,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flex(
+                direction: Axis.horizontal,
+                children: [
+                  _buildHeader(context),
+                ],
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      alignment: FractionalOffset.topCenter,
+                      image: NetworkImage(project.screenshots[1].image),
+                    ),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        stops: [0.0, 0.35],
+                        colors: [
+                          Colors.black.withAlpha(40),
+                          Colors.transparent
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
